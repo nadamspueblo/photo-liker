ace869bd37267c4b32ea0447d395da0a88c7b5f9
Nathan Adams <47441281+nadamspueblo@users.noreply.github.com>
2024-03-19 19:11:26 +0000

    Completed

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let likes = 0;
 
 function init() {
-  addClickEvent("thumbup", thumbUpClick)
+  addClickEvent("thumbup", thumbUpClick);
+  addClickEvent("thumbdown", thumbDownClick);
+  addClickEvent("addcomment", addComment);
 }
 
 function thumbUpClick() {
   likes += 1;
-  setText("Likes: " + likes);
+  setText("Likes: " + likes, "likes");
+}
+
+function thumbDownClick() {
+  likes -= 1;
+  setText("Likes: " + likes, "likes");
+}
+
+function addComment() {
+  let comment = getInputValue("newcomment");
+  let prevComments = getText("comments");
+  setText(prevComments + "<br>" + comment, "comments");
+  clearInputValue("newcomment");
 }
 
 init();
